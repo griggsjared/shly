5904fbe383a9ce886013960dee313898bc4cde95
Jared Griggs <griggsjared@gmail.com>
2025-02-15 12:45:18 -0700

    feat: add parse_input and use to split commands into argument tokens

--- main.c
+++ main.c
@@ -5,10 +5,41 @@
 #include <unistd.h> //fork, execlp, pid_t, wait
 
 #define MAX_INPUT_SIZE 1024
+#define MAX_PATH_SIZE 1024
+#define TOKEN_BUFFER_SIZE 64
+
+// parse_input takes a string input and a buffer size and returns an array of
+// strings (tokens)
+char **parse_input(char *input, int buffer_size) {
+  char **tokens = malloc(buffer_size * sizeof(char *));
+  if (tokens == NULL) {
+    fprintf(stderr,
+            "Memory allocation failed when parsing input into tokens\n");
+    return NULL;
+  }
+
+  char *token = strtok(input, " ");
+
+  int i = 0;
+  while (token != NULL) {
+    // TODO: increase buffer size if needed instead of returning NULL1
+    if (i >= buffer_size) {
+      fprintf(stderr, "Buffer size exceeded\n");
+      return NULL;
+    }
+
+    tokens[i] = token;
+    token = strtok(NULL, " ");
+    i++;
+  }
+  tokens[i] = NULL;
+
+  return tokens;
+}
 
 int main() {
   char input[MAX_INPUT_SIZE];
-  char cwd[PATH_MAX]; // 1024
+  char cwd[MAX_PATH_SIZE];
 
   while (1) {
 
@@ -29,39 +60,50 @@ int main() {
     // remove newline character
     input[strcspn(input, "\n")] = '\0';
 
+    // parse the entire input into argument tokens
+    char **args = parse_input(input, TOKEN_BUFFER_SIZE);
+    if (args == NULL) {
+      fprintf(stderr, "Failed to parse input into tokens\n");
+      return EXIT_FAILURE;
+    }
+
     // handle typing exit or quit
-    if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
+    if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "quit") == 0) {
       break;
     }
 
     // handle cd command
-    if (strcmp(input, "cd") == 0) {
-      if (chdir(getenv("HOME")) == -1) {
-        perror("chdir");
-      }
-      continue;
-    } else if (strncmp(input, "cd ", 3) == 0) {
-      if (chdir(input + 3) == -1) {
-        perror("chdir");
+    if (strcmp(args[0], "cd") == 0) {
+      // no arguments provided, we just go home
+      if (args[1] == NULL) {
+        if (chdir(getenv("HOME")) == -1) {
+          perror("chdir");
+        }
+      } else {
+        if (chdir(args[1]) == -1) {
+          perror("chdir");
+        }
       }
       continue;
     }
 
     // handle basic commands that can be handled through fork/exec
-    if (strlen(input) > 0) {
+    if (args[0] != NULL) {
       pid_t pid = fork();
       if (pid == 0) {
         // child process
-        execlp(input, input, NULL);
+        execvp(args[0], args);
         perror("exec");
         return EXIT_FAILURE;
       } else if (pid > 0) {
         wait(NULL);
       }
-    }
+    } 
+    
+    free(args);
   }
 
-  printf("Shly says goodbye!\n");
+  printf("shly says goodbye!\n");
 
   return EXIT_SUCCESS;
 }
