5cc78c5919656cb07f854aed89f4b0a4ca2aac3c
Jared Griggs <griggsjared@gmail.com>
2025-02-17 19:58:00 -0700

    style: format in builtins.c

--- src/builtins.c
+++ src/builtins.c
@@ -6,41 +6,44 @@
 #include "builtins.h" //builtin_command_t
 
 int handle_cd(char **args) {
-  if (strcmp(args[0], "cd") == 0) {
-    // no arguments provided, we just go home
-    if (args[1] == NULL) {
-      if (chdir(getenv("HOME")) == -1) {
-        perror("cd");
-      }
-    } else if (chdir(args[1]) == -1) {
+  if (strcmp(args[0], "cd") != 0) {
+    return 0;
+  }
+
+  if (args[1] == NULL) {
+    if (chdir(getenv("HOME")) == -1) {
       perror("cd");
     }
-    return 1;
+  } else if (chdir(args[1]) == -1) {
+    perror("cd");
   }
-  return 0;
+
+  return 1;
 }
 
 int handle_exit(char **args) {
   if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "quit") == 0) {
     return -1;
   }
+
   return 0;
 }
 
 int handle_builtins(char **args) {
   if (args[0] == NULL) {
-      return 0;
+    return 0;
   }
-  
+
   builtin_command_t builtins[] = {
-      {"cd", handle_cd},
-      {"exit", handle_exit},
-      {"quit", handle_exit},
-      {NULL, NULL} 
+    {"cd", handle_cd},
+    {"exit", handle_exit},
+    {"quit", handle_exit},
+    {NULL, NULL}
   };
-  
-  for(builtin_command_t *builtin = builtins; builtin->command != NULL; builtin++) {
-    if(strcmp(args[0], builtin->command) != 0) {
+
+  for (builtin_command_t *builtin = builtins; builtin->command != NULL;
+       builtin++) {
+    if (strcmp(args[0], builtin->command) != 0) {
       continue;
     }
 
