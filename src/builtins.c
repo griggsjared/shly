d37de29a7376fc5a9426baff06a567347e9b3644
Jared Griggs <griggsjared@gmail.com>
2025-02-17 19:05:11 -0700

    refactor: split builtins commands into own functions

--- src/builtins.c
+++ src/builtins.c
@@ -1,30 +1,36 @@
-#include <stdio.h> //perror
+#include <stdio.h>  //perror
 #include <stdlib.h> //getenv
 #include <string.h> //strcmp
 #include <unistd.h> //chdir
 
-int handle_builtins(char **args) {
-
-  // handle exit or quit command
-  if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "quit") == 0) {
-    return -1;
-  }
-
-  // handle cd command
+int handle_cd(char **args) {
   if (strcmp(args[0], "cd") == 0) {
     // no arguments provided, we just go home
     if (args[1] == NULL) {
       if (chdir(getenv("HOME")) == -1) {
-        perror("chdir");
+        perror("cd");
       }
-    } else {
-      if (chdir(args[1]) == -1) {
-        perror("chdir");
-      }
+    } else if (chdir(args[1]) == -1) {
+      perror("cd");
     }
-
     return 1;
   }
-
   return 0;
+}
+
+int handle_exit(char **args) {
+  if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "quit") == 0) {
+    return -1;
+  }
+  return 0;
+}
+
+int handle_builtins(char **args) {
+  int result = 0;
+  if (handle_cd(args) == 1) {
+    result = 1;
+  } else if (handle_exit(args) == -1) {
+    result = -1;
+  }
+  return result;
 }
--- src/builtins.h
+++ src/builtins.h
@@ -1,2 +1,9 @@
 //handle_builtins should return -1 if the program should exit, 1 if the program should continue, and 0 if the program should fork and exec the command
 int handle_builtins(char **args);
+
+//handle_cd should return 1 if the program should continue, and 0 if the program should fork and exec the command
+int handle_cd(char **args);
+
+//handle_exit should return -1 if the program should exit, and 0 if the program should fork and exec the command
+int handle_exit(char **args);
+
